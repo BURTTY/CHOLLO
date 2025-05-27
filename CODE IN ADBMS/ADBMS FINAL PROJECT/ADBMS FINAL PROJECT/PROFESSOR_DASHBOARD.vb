dfafee77dacb6d3539a877750e725a0978a4ba56
BURTTY <cholloasahan39@gmail.com>
2025-05-27 21:51:51 +0800

    Add files via upload

---
+++ CODE IN ADBMS/ADBMS FINAL PROJECT/ADBMS FINAL PROJECT/PROFESSOR_DASHBOARD.vb
@@ -0,0 +1,73 @@
+﻿Public Class PROFESSOR_DASHBOARD
+    Private Sub PROFESSOR_DASHBOARD_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub txtprofuser_TextChanged(sender As Object, e As EventArgs) Handles txtprofuser.TextChanged
+
+    End Sub
+
+    Private Sub txtprofpass_TextChanged(sender As Object, e As EventArgs) Handles txtprofpass.TextChanged
+
+    End Sub
+
+    Private Sub txtprofemail_TextChanged(sender As Object, e As EventArgs) Handles txtprofemail.TextChanged
+
+    End Sub
+
+    Private Sub txtprofcon_TextChanged(sender As Object, e As EventArgs) Handles txtprofcon.TextChanged
+
+    End Sub
+
+    Private Sub txtprofsec_TextChanged(sender As Object, e As EventArgs) Handles txtprofsec.TextChanged
+
+    End Sub
+
+    Private Sub cboprofyear_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboprofyear.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub txtprofcourse_TextChanged(sender As Object, e As EventArgs) Handles txtprofcourse.TextChanged
+
+    End Sub
+
+    Private Sub txtprofsubj_TextChanged(sender As Object, e As EventArgs) Handles txtprofsubj.TextChanged
+
+    End Sub
+
+    Private Sub txtprofprof_TextChanged(sender As Object, e As EventArgs) Handles txtprofprof.TextChanged
+
+    End Sub
+
+    Private Sub txtprofid_TextChanged(sender As Object, e As EventArgs) Handles txtprofid.TextChanged
+
+    End Sub
+
+    Private Sub txtprofgpa_TextChanged(sender As Object, e As EventArgs) Handles txtprofgpa.TextChanged
+
+    End Sub
+
+    Private Sub btnprofsave_Click(sender As Object, e As EventArgs) Handles btnprofsave.Click
+
+    End Sub
+
+    Private Sub btnprofback_Click(sender As Object, e As EventArgs) Handles btnprofback.Click
+
+    End Sub
+
+    Private Sub btnprofdel_Click(sender As Object, e As EventArgs) Handles btnprofdel.Click
+
+    End Sub
+
+    Private Sub btnprofexit_Click(sender As Object, e As EventArgs) Handles btnprofexit.Click
+
+    End Sub
+
+    Private Sub btnprofbatch_Click(sender As Object, e As EventArgs) Handles btnprofbatch.Click
+
+    End Sub
+End Class
